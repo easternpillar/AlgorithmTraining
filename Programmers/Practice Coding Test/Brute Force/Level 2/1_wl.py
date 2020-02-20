7542434e30e6308829cfc63666cdf97adffe3414
easternpillar <roh941129@gmail.com>
2020-02-20 14:22:55 +0900

    2020.02.20 ~ Brute Force Level 2: Problem 1.

---
+++ Programmers/Practice Coding Test/Brute Force/Level 2/1_wl.py
@@ -0,0 +1,38 @@
+# Problem:
+# Given numbers from 0 to 9, return the number of prime numbers that can be made by permutations.
+# Condition(s):
+
+# My Solution:
+import itertools
+
+
+def solution(numbers):
+    answer = 0
+    strings = []
+
+    for i in range(1, len(numbers) + 1):
+        perm = list(itertools.permutations(numbers, i))
+        strings.extend([*perm])
+
+    strings = list(map(list, strings))
+    for i in range(len(strings)):
+        strings[i] = "".join(strings[i])
+        strings[i] = int(strings[i])
+
+    strings = sorted(list(set(strings)))
+
+    for i in range(len(strings)):
+        prime = 1
+        if strings[i] <= 1:
+            continue
+        for div in range(2, strings[i], 1):
+            if strings[i] % div == 0:
+                prime = 0
+                break
+        if prime == 1:
+            answer += 1
+
+    return answer
+
+# Learned:
+# 1. itertools: module that makes permutations or combinations.
