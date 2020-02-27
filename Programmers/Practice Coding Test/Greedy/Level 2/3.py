6c9b80ca475c69fd0ba5eb686771007b6e0a5ddf
easternpillar <roh941129@gmail.com>
2020-02-27 18:20:41 +0900

    2020.02.27 ~ Greedy Level 2: Problem 3.

---
+++ Programmers/Practice Coding Test/Greedy/Level 2/3.py
@@ -0,0 +1,20 @@
+# Problem:
+# Given wieghts of people and limit weight of boat, return how many boats will be needed.
+# Condition(s):
+# 1. The limit of passengers is 2 people.
+
+def solution(people, limit):
+    answer = 0
+    people.sort(reverse=True)
+    front = 0
+    back = len(people) - 1
+
+    while front <= back:
+        if people[front] + people[back] <= limit:
+            front += 1
+            back -= 1
+        else:
+            front += 1
+        answer += 1
+
+    return answer
