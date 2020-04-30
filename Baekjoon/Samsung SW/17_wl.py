2cce374377f50f94a02ebfdb0ff014e07c57c546
easternpillar <roh941129@gmail.com>
2020-04-30 13:32:04 +0900

    2020.04.30 ~ Samsung SW: Problem 17.

--- Baekjoon/Samsung SW/17_wl.py
+++ Baekjoon/Samsung SW/17_wl.py
@@ -4,38 +4,39 @@
 # My Solution:
 import itertools
 
-def distance(city,remains):
-    r_list=list(remains)
 
-    tot=0
+def distance(city, remains):
+    r_list = list(remains)
+
+    tot = 0
     for i in range(len(city)):
         for j in range(len(city[i])):
-            if city[i][j]==1:
-                min=9999
+            if city[i][j] == 1:
+                min = 9999
                 for r in remains:
-                    if abs(i-r[0])+abs(j-r[1])<min:
-                        min=abs(i-r[0])+abs(j-r[1])
-                tot+=min
+                    if abs(i - r[0]) + abs(j - r[1]) < min:
+                        min = abs(i - r[0]) + abs(j - r[1])
+                tot += min
 
     return tot
 
 
-n,m=map(int,list(input().split()))
-city=[list(map(int,list(input().split()))) for _ in range(n)]
+n, m = map(int, list(input().split()))
+city = [list(map(int, list(input().split()))) for _ in range(n)]
 
-chk_list=[]
+chk_list = []
 for i in range(len(city)):
     for j in range(len(city[i])):
-        if city[i][j]==2:
-            chk_list.append([i,j])
+        if city[i][j] == 2:
+            chk_list.append([i, j])
 
-if len(chk_list)-m<=0:
-    print(distance(city,set(map(tuple,(chk_list)))))
+if len(chk_list) - m <= 0:
+    print(distance(city, set(map(tuple, (chk_list)))))
 else:
-    answer=set()
-    for removes in list(map(list,itertools.combinations(chk_list,len(chk_list)-m))):
-        remains=set(map(tuple,(chk_list)))-set(map(tuple,removes))
-        answer.add(distance(city,remains))
+    answer = set()
+    for removes in list(map(list, itertools.combinations(chk_list, len(chk_list) - m))):
+        remains = set(map(tuple, chk_list)) - set(map(tuple, removes))
+        answer.add(distance(city, remains))
 
     print(min(answer))
 
