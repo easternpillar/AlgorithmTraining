f20140947942429ae824e6a8f7812005ddd95da6
easternpillar <roh941129@gmail.com>
2021-08-12 04:12:14 +0900

    2021.8.12. ~ Baekjoon: 1 Problem(s).

--- Baekjoon/트리/트리인가.py
+++ Baekjoon/트리/트리인가.py
@@ -3,38 +3,66 @@
 
 # My Solution:
 import sys
-from collections import defaultdict, Counter
+from collections import defaultdict, deque
 
-idx = 0
-while True:
-    quit = sys.stdin.readline().rstrip().split()
 
-    if quit:
-        if int(quit[0]) < 0 and int(quit[1]) < 0:
-            break
-
-    indegree = defaultdict(int)
-    temp = []
-    temp += quit
-    idx += 1
-    while True:
-        temp += list(map(int, sys.stdin.readline().rstrip().split()))
-
-        if temp[-1] == 0 and temp[-2] == 0:
-            break
-    if len(temp) == 2:
-        print("Case", idx, "is a tree.")
-        continue
-
-    for i in range(0, len(temp) - 2, 2):
-        if temp[i] not in indegree.keys():
-            indegree[temp[i]] = 0
-        if temp[i + 1] not in indegree.keys():
-            indegree[temp[i + 1]] = 0
-        indegree[temp[i + 1]] += 1
-    counter = Counter(indegree.values())
-
-    if counter[0] != 1 or not (len(counter.keys()) <= 2 and sum(counter.keys()) == 1):
-        print("Case", idx, "is not a tree.")
+def bfs(start):
+    q = deque([start])
+    visited = {}
+    for key in nodes:
+        visited[key] = False
+    while q:
+        pop = q.popleft()
+        if visited[pop]:
+            return False
+        visited[pop] = True
+        for con in conn[pop]:
+            q.append(con)
+    if False in visited.values():
+        return False
     else:
-        print("Case", idx, "is a tree.")
+        return True
+
+
+case = 0
+seq = []
+while True:
+    temp = list(map(int, sys.stdin.readline().split()))
+    if len(temp) == 2 and temp[0] < 0 and temp[1] < 0:
+        break
+    else:
+        seq.extend(temp)
+        if len(seq) >= 2:
+            if seq[-1] == 0 and seq[-2] == 0:
+                case += 1
+                seq = seq[:len(seq) - 2]
+
+                if not seq:
+                    print("Case", case, "is a tree.")
+                    seq = []
+                    continue
+
+                nodes = set(seq)
+                conn = defaultdict(list)
+                indegree = defaultdict(int)
+                for i in range(0, len(seq), 2):
+                    conn[seq[i]].append(seq[i + 1])
+                    if seq[i] not in indegree.keys():
+                        indegree[seq[i]] = 0
+                    if seq[i + 1] not in indegree.keys():
+                        indegree[seq[i + 1]] = 0
+                    indegree[seq[i + 1]] += 1
+
+                root = []
+                for key in indegree.keys():
+                    if indegree[key] == 0:
+                        root.append(key)
+
+                if len(root) > 1 or len(root) == 0:
+                    print("Case", case, "is not a tree.")
+                else:
+                    if bfs(root[0]):
+                        print("Case", case, "is a tree.")
+                    else:
+                        print("Case", case, "is not a tree.")
+                seq = []
